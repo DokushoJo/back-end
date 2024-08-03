a5bf7d3c4f7de2cd6e59447870686f9737240917
Robert Bolgeo <166631820+robertbolgeo@users.noreply.github.com>
2024-08-03 20:19:59 +0900

    Feature: Readded CORS.

--- src/index.ts
+++ src/index.ts
@@ -9,10 +9,16 @@ dotenv.config();
 const app: Express = express();
 const port = process.env.PORT || 3000;
 
+const allowedOrigins = ['http://localhost:5173'];
+
+const options: cors.CorsOptions = {
+  origin: allowedOrigins
+};
+
 //app use
 app.use(express.json());
-app.use(cors({origin: "http://localhost:5173"}));
 app.use(router);
+app.use(cors(options))
 
 app.listen(port, () => {
   console.log(`[server]: Server is running at http://localhost:${port}`);
